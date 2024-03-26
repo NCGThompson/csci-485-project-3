7e9f9655ca22c8b977c8389e65718dc14621062d
Nicholas Thompson <NCGThompson@gmail.com>
2024-03-25 23:46:28 -0400

    added scraping.rs and edited Cargo.toml

---
+++ src/scraping.rs
@@ -0,0 +1,18 @@
+use std::path;
+
+// use rust_search;
+
+/// Searches for the files we need, `special_file.txt` and `secret_file.txt`
+/// and returns there locations if found each as a `PathBuf` in the order listed here.
+/// This uses the library `rust_search` which indirectly wraps `walkdir`.
+///
+/// `scrape()` will always search the running users home directory if possible.
+/// Iff it still hasn't found one or more of the files, and the `sudoer`
+/// feature is enabled, it will search the root directory next.
+///
+/// This function is optomized for Ubuntu because that is the target,
+/// but ideally it should be cross platform so anyone can easily test it on their
+/// local machine.
+pub fn find_files() -> (Option<path::PathBuf>, Option<path::PathBuf>) {
+    (None, None)
+}
