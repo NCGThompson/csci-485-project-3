0173d0fc50b4bd9791e0f0f0d94104d5440390ea
Nicholas Thompson <NCGThompson@gmail.com>
2024-03-25 22:32:51 -0400

    move most functions to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+use std::fs;
+use std::io::{self, Read};
+use openssl::symm::{decrypt, Cipher};
+//not sure if a remote server exist got to go back and reread the document.
+const REMOTE_SERVER_URL: &str = "http://example.com/upload";
+
+pub fn find_file(file_name: &str) -> Result<String, io::Error> {
+    // Implement file scraping here, recursively traverse file system to find the file
+    // Return the path to the file
+    Ok(file_name.to_owned())
+}
+
+pub fn read_file(file_path: &str, buffer: &mut Vec<u8>) -> Result<(), io::Error> {
+    let mut file = fs::File::open(file_path)?;
+    file.read_to_end(buffer)?;
+    Ok(())
+}
+
+pub fn decrypt_file(data: &[u8], key: &[u8]) -> Result<Vec<u8>, openssl::error::ErrorStack> {
+    let cipher = Cipher::aes_256_cbc();
+    let decrypted_data = decrypt(cipher, key, None, data)?;
+    Ok(decrypted_data)
+}
+
+pub fn send_to_remote_server(data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+    // Implement HTTP request to send data to remote server
+    let client = reqwest::blocking::Client::new();
+    client.post(REMOTE_SERVER_URL)
+        .body(data.to_vec())
+        .send()?;
+    Ok(())
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 // basic outline for this program. Hello, Nic and Simeon here is what I have so far.
-use std::fs;
-use std::io::{self, Read};
-use openssl::symm::{decrypt, Cipher};
+use libproj3::{decrypt_file, find_file, read_file, send_to_remote_server};
 //not sure if a remote server exist got to go back and reread the document.
-const REMOTE_SERVER_URL: &str = "http://example.com/upload";
 const SPECIAL_FILE_NAME: &str = "special_file.txt";
 const SECRET_FILE_NAME: &str = "secret_file.txt";
 
@@ -31,30 +28,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn find_file(file_name: &str) -> Result<String, io::Error> {
-    // Implement file scraping here, recursively traverse file system to find the file
-    // Return the path to the file
-    Ok(file_name.to_owned())
-}
-
-fn read_file(file_path: &str, buffer: &mut Vec<u8>) -> Result<(), io::Error> {
-    let mut file = fs::File::open(file_path)?;
-    file.read_to_end(buffer)?;
-    Ok(())
-}
-
-fn decrypt_file(data: &[u8], key: &[u8]) -> Result<Vec<u8>, openssl::error::ErrorStack> {
-    let cipher = Cipher::aes_256_cbc();
-    let decrypted_data = decrypt(cipher, key, None, data)?;
-    Ok(decrypted_data)
-}
-
-fn send_to_remote_server(data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
-    // Implement HTTP request to send data to remote server
-    let client = reqwest::blocking::Client::new();
-    client.post(REMOTE_SERVER_URL)
-        .body(data.to_vec())
-        .send()?;
-    Ok(())
-}
 // wiggle sauce my goats
